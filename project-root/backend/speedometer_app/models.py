cfa3f3bf86377422fecbabf8733d065f13c2d081
Hir <119331222+hp1801@users.noreply.github.com>
2024-12-04 17:13:53 +0530

    Delete project-root directory

--- project-root/backend/speedometer_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class SpeedData(models.Model):
-    timestamp = models.DateTimeField(auto_now_add=True)
-    speed = models.FloatField()
-
-    def __str__(self):
-        return f"{self.timestamp} - {self.speed}"
-
--- project-root/backend/speedometer_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import get_latest_speed
-
-urlpatterns = [
-    path('speed/', get_latest_speed, name='get-speed'),
-]
